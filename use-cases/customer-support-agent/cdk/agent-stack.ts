0cfceb620115704953a930ff8b0c6c62f12b043c
GengisAWS <gengis@amazon.com>
2026-01-19 16:15:31 +0100

    refactor: use DockerImageAsset for automatic build/push

--- use-cases/customer-support-agent/cdk/agent-stack.ts
+++ use-cases/customer-support-agent/cdk/agent-stack.ts
@@ -1,14 +1,19 @@
 import * as cdk from 'aws-cdk-lib'
+import * as ecr_assets from 'aws-cdk-lib/aws-ecr-assets'
 import * as iam from 'aws-cdk-lib/aws-iam'
 import { Construct } from 'constructs'
+import * as path from 'node:path'
+import * as url from 'node:url'
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
 
 export class AgentStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
 
-    const imageUri = new cdk.CfnParameter(this, 'ContainerImageUri', {
-      type: 'String',
-      description: 'ECR image URI',
+    const imageAsset = new ecr_assets.DockerImageAsset(this, 'AgentImage', {
+      directory: path.join(__dirname, '..'),
+      platform: ecr_assets.Platform.LINUX_ARM64,
     })
 
     const runtimeRole = new iam.Role(this, 'RuntimeRole', {
@@ -45,7 +50,7 @@ export class AgentStack extends cdk.Stack {
         AgentRuntimeName: 'customer_support_multiagent',
         AgentRuntimeArtifact: {
           ContainerConfiguration: {
-            ContainerUri: imageUri.valueAsString,
+            ContainerUri: imageAsset.imageUri,
           },
         },
         ProtocolConfiguration: 'HTTP',
